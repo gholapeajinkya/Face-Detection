0722fecbc95e8178127280fc648b298c0ca25dac
Ajinkya Gholape <38329349+ajinkyagholape1998@users.noreply.github.com>
2019-06-24 10:31:54 +0530

    Update test.py

--- Face Detection Gui/test.py
+++ Face Detection Gui/test.py
@@ -3,18 +3,39 @@ from PIL import Image, ImageTk
 from tkinter import filedialog
 import numpy as np
 import cv2
-import datetime
 from tkinter import messagebox
 
 def getImage():
     filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
     return filename
 
-def destroyPanel():
-    panel.destroy()
-    panel.pack_forget()
-    print("control in destroyPanel function\n")
+def sel(x):
+    try:
+        global selection
+        selection = var.get()
+        img = cv2.imread(filename)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        cascPath = "haarcascade_frontalface_default.xml"
+        faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + cascPath)
+        faces = faceCascade.detectMultiScale(gray,selection,5)
+        for (i,(x, y, w, h)) in enumerate(faces):
+            cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            cv2.putText(img, "{}#".format(i + 1), (x - 4, y - 10),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 255, 0), 2)
 
+        global image
+        image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = Image.fromarray(image)
+        img = img.resize((500, 400), Image.ANTIALIAS)
+        img = ImageTk.PhotoImage(img)
+        global panel
+        panel = Label(window, image = img)
+        panel.image = img
+        panel.place(x=0, y=0)	
+    except:
+        pass
+
+    
 def saveImage():
     try:
         img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -22,13 +43,15 @@ def saveImage():
         print(file)
         cv2.imwrite(file,img)
         print("image saved")
+        messagebox.showinfo("Image Saved", "Image Saved at "+file)
     except:
-        messagebox.showinfo("Message", "first browse an image using Browse Image button")
+        pass
 def proceessImage():
-    messagebox.showinfo("Message", "If you are pressing this button for second time, first press the Delete Image button")
+    global filename
     filename = getImage()
     print(filename)
-    if filename:                                   
+    if filename:
+        global img
         img = cv2.imread(filename)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         cascPath = "haarcascade_frontalface_default.xml"
@@ -37,20 +60,23 @@ def proceessImage():
         for (i,(x, y, w, h)) in enumerate(faces):
             cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
             cv2.putText(img, "{}#".format(i + 1), (x - 4, y - 10),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 255, 0), 2)
+	    cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 255, 0), 2)
 
+        global var
+        var = DoubleVar()
+        scale = Scale(window,resolution=0.01,variable = var ,orient=HORIZONTAL,command = sel, length=300,from_=0.0, to=2.0)
+        scale.place(x=90, y=420)
+        scale.set(1.1)
+        
         global image
         image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        
         img = Image.fromarray(image)
         img = img.resize((500, 400), Image.ANTIALIAS)
         img = ImageTk.PhotoImage(img)
         global panel
         panel = Label(window, image = img)
         panel.image = img
-        panel.place(x=0, y=0)
-        panel.pack()
-        
+        panel.place(x=0, y=0)	        
 
 if __name__ == '__main__':
     window = Tk()
@@ -59,13 +85,10 @@ if __name__ == '__main__':
     
     btn1 = Button(window, text = "Browse Image",activebackground="#2896F6",command=proceessImage)
     btn1.pack()
-    btn1.place(x=100,y=450)
-    btn2 = Button(window, text = "Delete Image",activebackground="#2896F6",command=destroyPanel)
-    btn2.pack()
-    btn2.place(x=200,y=450)
+    btn1.place(x=100,y=500)
     btn3 = Button(window, text = "Save Image",activebackground="#2896F6",command=saveImage)
     btn3.pack()
-    btn3.place(x=300,y=450)
-    window.geometry("505x500")
+    btn3.place(x=300,y=500)
+    window.geometry("505x550")
     window.resizable(False, False)
     window.mainloop()
